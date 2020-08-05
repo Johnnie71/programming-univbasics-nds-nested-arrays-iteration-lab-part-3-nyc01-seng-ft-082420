e5b8035e7092191e0f305c6366d5ffb4d7780c2f
Johnnie Gonzalez <johnnie.agonz71@gmail.com>
2020-08-05 20:43:14 +0000

    Automatically backed up by Learn

--- lib/iteration_with_loops.rb
+++ lib/iteration_with_loops.rb
@@ -1,17 +1,17 @@
-def join_nested_strings(src)
-  string_data = []
-  row_index = 0 
+# def join_nested_strings(src)
+#   string_data = []
+#   row_index = 0 
   
-  while row_index < src.count do
-    element_index = 0 
+#   while row_index < src.count do
+#     element_index = 0 
     
-    while element_index < src[row_index][element_index].count do
-      if src[row_index][element_index].class == String
-        string_data << src[row_index][element_index].join + " " 
-      end
-      element_index += 1 
-    end
-    row_index += 1 
-  end
-  string_data
-end
+#     while element_index < src[row_index][element_index].count do
+#       if src[row_index][element_index].class == String
+#         string_data << src[row_index][element_index].join + " " 
+#       end
+#       element_index += 1 
+#     end
+#     row_index += 1 
+#   end
+#   string_data
+# end
